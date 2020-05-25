c8cd5f9532aac8d8d5567950d2ea13ea844e35a5
Harish <harishbommakanti2002@gmail.com>
2020-05-25 18:44:22 -0500

    added out/ to .gitignore and solved problem 1

---
+++ src/Problem1.java
@@ -0,0 +1,11 @@
+public class Problem1
+{
+    //Find the sum of all the multiples of 3 or 5 below 1000.
+    public static void main(String[] args)
+    {
+        int sum=0;
+        for (int i = 1; i < 1000; i++)
+            if (i%3==0 || i%5==0) sum+=i;
+        System.out.println(sum);
+    }
+}
