dd2fe0ace1801cb23dabb04d488b8a7db67d196a
Harish <harishbommakanti2002@gmail.com>
2020-06-01 09:56:51 -0500

    finished problem 13 using BigInteger

--- src/Problem13.java
+++ src/Problem13.java
@@ -3,10 +3,39 @@ Work out the first ten digits of the sum of the following one-hundred 50-digit n
 (numbers are in Problem13Data.txt
  */
 
+import java.io.File;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Problem13
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
+        String[] nums = readData();
+        //System.out.println(Arrays.toString(nums));
 
+        BigInteger sum = new BigInteger("0");
+        for (String i:nums)
+            sum = sum.add(new BigInteger(i));
+
+        System.out.println(sum.toString().substring(0,10)); //only need the first 10 digits
+    }
+
+    private static String[] readData() throws IOException
+    {
+        String fpath = "src/Problem13Data.txt";
+        Scanner s = new Scanner(new File(fpath));
+        String[] ret = new String[100];
+
+        int i = 0;
+        while(s.hasNextLine())
+        {
+            ret[i] = s.nextLine();
+            i++;
+        }
+
+        return ret;
     }
 }
