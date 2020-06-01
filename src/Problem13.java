6cf7d94c34b3df146549f1c22041ca610be29607
Harish <harishbommakanti2002@gmail.com>
2020-06-01 09:35:54 -0500

    added files for problem 13

---
+++ src/Problem13.java
@@ -0,0 +1,12 @@
+/*
+Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+(numbers are in Problem13Data.txt
+ */
+
+public class Problem13
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
