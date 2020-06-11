568e91de0caafd42abdcce432a7b9f7d667eff00
Harish <harishbommakanti2002@gmail.com>
2020-06-11 12:37:39 -0500

    added some comments for prob24

--- src/Java/Problem24.java
+++ src/Java/Problem24.java
@@ -35,9 +35,17 @@ public class Problem24
             permutations.add(prefix); //result is when the prefix has 10 letters and str has 0
         else
         {
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) //iterate over all indices of str
             {
+                //new prefix will be the prefix until now with the current iterated character added on,
+                //str will be what the string was before, besides that 1 character
+
+                //so itll start from prefix="", str = "0123456789" to prefix = "0","1"... and str = "12345..","02..","013..." etc respectively
                 permute(prefix + str.charAt(i),str.substring(0,i) + str.substring(i+1));
+
+                //this guarantees that the length of str gets smaller and smaller with further recursive calls, adding the removed character to prefix
+
+                //it guarantees all possible combos because if a call is prefix="0" and str="123456789", all/only letters from str will be added to prefix
             }
         }
     }
