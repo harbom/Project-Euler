f0f5c8951067e726f22ecb3fac5c16a76fcfcb9c
Harish <harishbommakanti2002@gmail.com>
2020-06-12 14:04:25 -0500

    wrote up problem 26 in python, didn't work because int overflow so java was superior here

---
+++ src/Python/Problem26.py
@@ -0,0 +1,46 @@
+"""
+A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to 10 are given:
+    1/2	= 	0.5
+    1/3	= 	0.(3)
+    1/4	= 	0.25
+    1/5	= 	0.2
+    1/6	= 	0.1(6)
+    1/7	= 	0.(142857)
+    1/8	= 	0.125
+    1/9	= 	0.(1)
+    1/10	= 	0.1
+Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
+Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+"""
+def factorize2and5Out(n: int) -> int:
+    while n%2==0 or n%5==0:
+        if n%2==0:
+            n /= 2
+        if n%5==0:
+            n /= 5
+    
+    return n
+
+def main():
+    maxlen = 0
+    num = 0
+    max = 1000
+
+    for i in range(3,max):
+        factorized = factorize2and5Out(i)
+
+        if factorized != 1:
+            k=1
+
+            while((10**k % factorized) != 1): #error here, int overflow
+                k+=1
+            
+            if k>maxlen:
+                maxlen = k
+                num = i
+    
+    print(num)
+
+
+if __name__ == '__main__':
+    main()
