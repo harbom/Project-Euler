e84b8824fa16a682f87fd8f6bde0710fdb0aeb06
Harish <harishbommakanti2002@gmail.com>
2020-06-15 08:45:33 -0500

    finished problem 27

--- src/Python/Problem27.py
+++ src/Python/Problem27.py
@@ -16,8 +16,38 @@ where |n| is the modulus/absolute value of n e.g. |11|=11 and |−4|=4
 Find the product of the coefficients, a
 and b, for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n=0.
 """
+import math
+
+def f(a:int,b:int, n:int) -> int:
+    return n**2 + a*n + b
+
+def isPrimeNaive(n: int) -> bool:
+    if n<0:
+        return False #negative numbers aren't prime, additional factor of -1
+
+    for i in range(2,int(math.sqrt(n)+1)): #only factors should be 1 and n, not anything in [2,sqrt(n)]
+        if n % i == 0:
+            return False
+    
+    return True
+
 def main():
-    pass
+    maxlen=0
+    maxprod=0
+
+    for a in range(-999,1000):
+        for b in range(-1000,1001):
+            currlen = 0
+            n = 0
+            while(isPrimeNaive(f(a,b,n))):
+                currlen += 1
+                n += 1
+            
+            if currlen > maxlen:
+                maxlen = currlen
+                maxprod = a*b
+    
+    print(maxprod)
 
 if __name__ == '__main__':
     main()
