5898280488b9fd2bf7b4a05d7857a800f9c6effa
Harish <harishbommakanti2002@gmail.com>
2020-06-15 11:49:57 -0500

    finished problem 30

--- src/Python/Problem30.py
+++ src/Python/Problem30.py
@@ -11,3 +11,20 @@ The sum of these numbers is 1634 + 8208 + 9474 = 19316.
 
 Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
 """
+
+def canBeWritten(n: int) -> bool:
+    powersum = sum([int(i)**5 for i in str(n)])
+    return powersum == n
+
+def main():
+    upper = 999999
+    nums = list()
+    for num in range(11,upper+1):
+        if canBeWritten(num):
+            nums.append(num)
+    
+    print(sum(nums))
+
+
+if __name__ == "__main__":
+    main()
