773afbdf0e5ede8e88ab4258ee98238dff449761
Harish <harishbommakanti2002@gmail.com>
2020-06-17 10:30:13 -0500

    finished problem 34

--- src/Python/Problem34.py
+++ src/Python/Problem34.py
@@ -6,8 +6,31 @@ Find the sum of all numbers which are equal to the sum of the factorial of their
 Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 """
 
+def factorial(n: int) -> int:
+    if n==0:
+        return 1
+    
+    ret=1
+    while(n>=1):
+        ret*=n
+        n-=1
+    
+    return ret
+
+def doesqualify(n: int) -> bool:
+    factsum = 0
+    for i in str(n):
+        factsum += factorial(int(i))
+    
+    return factsum==n
+
 def main():
-    pass
+    ans=0
+    for i in range(10,999999):
+        if doesqualify(i):
+            ans += i
+    
+    print(ans)
 
 if __name__ == '__main__':
     main()
