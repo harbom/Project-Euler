37dfb75d49ce30744574559f9f1722287bbe60f2
Harish <harishbommakanti2002@gmail.com>
2020-06-17 11:35:54 -0500

    wrote code for problem 35 in python but TLE... i'll see if it works in java or else i'll change the algo

--- src/Python/Problem35.py
+++ src/Python/Problem35.py
@@ -6,8 +6,47 @@ There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73
 How many circular primes are there below one million?
 """
 
+def sieveOfErat(upperlimit: int) -> list:
+    primes = list()
+    booltable = [True for i in range(upperlimit + 1)]
+
+    for i in range(2,upperlimit):
+        if booltable[i] == False: #you know its a composite
+            continue
+
+        #if its gotten this far, its a prime
+        primes.append(i)
+
+        #mark all multiples or current primes until upperlimit as composite
+        for j in range(2*i,upperlimit,i): #step by i
+            booltable[j] = False
+    
+    return primes
+
 def main():
-    pass
+    #can first do the sieve of erat to narrow down the candidates
+    upperlimit=1000000
+    listprimes = sieveOfErat(upperlimit)
+    numcircprimes = 0
+
+    for num in listprimes:
+        currstr = str(num)
+        currlen = len(currstr)
+        
+        
+        areRotationsPrimeList = list()
+        for i in range(currlen):
+            #197, 971, 719: 197 is original string so its str[i:] + str[:i] to find all rotations
+            currrotation = currstr[i:] + currstr[:i]
+            
+            areRotationsPrimeList.append(int(currrotation) in listprimes) #boolean
+        
+        if all(areRotationsPrimeList):
+            numcircprimes += 1
+    
+    print(numcircprimes)
+    
+
 
 if __name__ == '__main__':
     main()
