cc656b9d8c2af0c88a9b9afd7413a2d055b2daee
Harish <harishbommakanti2002@gmail.com>
2020-06-22 09:12:44 -0500

    finished problem 37

--- src/Python/Problem37.py
+++ src/Python/Problem37.py
@@ -5,9 +5,40 @@ Find the sum of the only eleven primes that are both truncatable from left to ri
 
 NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 """
+from Problem35 import sieveOfErat #returns the list of primes from 2..upper using sieve of erat
 
 def main():
-    pass
+    upper = 1000000
+    lstprimes = sieveOfErat(upper)
+
+    ans = 0
+    for num in lstprimes:
+        if num in (2,3,5,7): #problem restriction
+            continue
+        currstr = str(num)
+        
+        allareprime = True #flag
+        #left to right
+        for i in range(len(currstr)):
+            currnum = int(currstr[i:])
+            if currnum not in lstprimes: #if curr truncation is not prime, break
+                allareprime = False
+                break
+        
+        if not(allareprime): #break at this pt, no need for right to left
+            continue
+
+        #right to left
+        for i in range(len(currstr)-1,-1,-1): #backwards from last char to first char
+            currnum = int(currstr[:i+1])
+            if currnum not in lstprimes:
+                allareprime = False
+                break
+        
+        if allareprime:
+            ans+=num
+    
+    print(ans)
 
 if __name__ == '__main__':
     main()
