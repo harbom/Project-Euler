f07137e47d3d79b3bcdd1de0ee352cb12c0360d0
Harish <harishbommakanti2002@gmail.com>
2020-06-23 10:01:38 -0500

    finished problem 38 using reasonable bounds

--- src/Python/Problem38.py
+++ src/Python/Problem38.py
@@ -11,9 +11,36 @@ The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5
 
 What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1?
 """
+def isStringPandigital(n: str) -> bool:
+    if len(n) != 9:
+        return False
+    
+    for i in range(1,10):
+        if n.count(str(i)) != 1:
+            return False
+    return True
 
 def main():
-    pass
+    #try to do it with reasonable bounds
+    ans = 0
+
+    nrange = 5
+    for n in range(2,nrange):
+
+        numrange = 10000
+        for num in range(numrange):
+            currstr = ""
+
+            for i in range(1,n+1):
+                currstr += str(num*i)
+            
+            if isStringPandigital(currstr):
+                if int(currstr) > ans:
+                    ans = int(currstr)
+    
+    print(ans)
+            
+
 
 if __name__ == "__main__":
     main()
