ca918d048c47532b4f75487a159bff39baeb5171
Harish <harishbommakanti2002@gmail.com>
2020-06-24 11:52:30 -0500

    finished problem 40

--- src/Python/Problem40.py
+++ src/Python/Problem40.py
@@ -11,7 +11,15 @@ d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
 """
 
 def main():
-    pass
+    string = str()
+    for i in range(1,1000001): #populate the string
+        string += str(i)
+    
+    #get the answer
+    ans = int(string[0])
+    for i in range(1,7):
+        ans *= int(string[int('9'*i)]) #so it goes string[9],string[99]...string[999999]
+    print(ans)
 
 if __name__ == "__main__":
     main()
