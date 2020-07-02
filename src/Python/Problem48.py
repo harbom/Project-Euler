f61f1cab032ac9e0d916a8fb9760404524dfe55f
Harish <harishbommakanti2002@gmail.com>
2020-07-02 18:42:53 -0500

    way more efficient way of doing problem 48, used modular magic (link in the code)

--- src/Python/Problem48.py
+++ src/Python/Problem48.py
@@ -6,11 +6,27 @@ Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 
 def main():
     #try naive way
+    """
     val = 0
     for i in range(1,1001):
         val += i**i
     
     print(str(val)[-10:])
+    """
+
+    #modular math way: https://www.mathblog.dk/project-euler-48-last-ten-digits/
+    val = 0
+    modulo = 10000000000
+    for i in range(1,1001):
+        temp = i
+        for j in range(1,i):
+            temp *= i
+            temp %= modulo
+        
+        val += temp
+        val %= modulo
+    
+    print(val)
 
 if __name__ == "__main__":
     main()
