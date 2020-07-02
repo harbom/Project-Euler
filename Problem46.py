6a4a9379d23677c4214e9d8a3c3c16e2a5dac1f8
Harish <harishbommakanti2002@gmail.com>
2020-07-02 13:42:37 -0500

    finished problem 46 in python

--- Problem46.py
+++ Problem46.py
@@ -13,8 +13,48 @@ It turns out that the conjecture was false.
 What is the smallest odd composite that cannot be written as the sum of a prime and twice a square?
 """
 
+from src.Python.Problem27 import isPrimeNaive
+from src.Python.Problem35 import sieveOfErat
+
 def main():
-    pass
+    currnum = 33
+
+    while(True): #keep on going until something is found
+        if isPrimeNaive(currnum):
+            currnum += 2
+            continue
+
+        print("currnum: ",currnum)
+        primelist = sieveOfErat(currnum) #get list of primes until that #
+        #print("primelist: ",primelist)
+        flag = False
+
+        for currprime in primelist[::-1]: #go from highest to lowest to possibly reduce time
+            currsum = currprime
+            difference = int((currnum - currsum) / 2) #liberal guess based on pattern, allows space to guess for the squared term
+
+            bigbreak = False
+            for i in range(difference+5): #+5 just because why not lol
+                if currsum + 2 * (i**2) == currnum: #its not the answer, we know this so avoid further reps
+                    flag = True
+                    bigbreak = True
+                    print("Can be written: %d + 2*%d^2" % (currprime,i))
+                    break
+            
+            if bigbreak:
+                break
+
+        print()
+
+        
+        #if not(flag) --> never found a pair of prime + 2*(square)
+        if not(flag):
+            print("ans: ",currnum)
+            break
+        else:
+            currnum += 2
+            
+
 
 if __name__ == "__main__":
     main()
