c08c3ee7aea89db92e812ae08ccad29c8af1c293
Harish <harishbommakanti2002@gmail.com>
2020-07-02 18:16:41 -0500

    finished problem 48 in python

--- src/Python/Problem48.py
+++ src/Python/Problem48.py
@@ -5,7 +5,12 @@ Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 """
 
 def main():
-    pass
+    #try naive way
+    string = "0"
+    for i in range(1,1001):
+        string = str(int(string)+i**i)
+    
+    print(string[-10:])
 
 if __name__ == "__main__":
     main()
