fcf0b7b64cf5c87db070fbfa6d51cd0c3c84af05
Harish <harishbommakanti2002@gmail.com>
2020-07-02 18:30:54 -0500

    clearer way of Problem48.py, using strings was uneeded

--- src/Python/Problem48.py
+++ src/Python/Problem48.py
@@ -6,11 +6,11 @@ Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 
 def main():
     #try naive way
-    string = "0"
+    val = 0
     for i in range(1,1001):
-        string = str(int(string)+i**i)
+        val += i**i
     
-    print(string[-10:])
+    print(str(val)[-10:])
 
 if __name__ == "__main__":
     main()
